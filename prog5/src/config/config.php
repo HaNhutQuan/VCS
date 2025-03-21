95bd3a76a8d9a68a0df465d8ae4ef362333b1076
Quang Hà <you@example.com>
2025-03-21 21:05:14 +0700

    Update

--- prog5/src/config/config.php
+++ prog5/src/config/config.php
@@ -11,8 +11,8 @@ return [
         "url"   => "http://localhost:1200"
     ],
     "cloudinary" => [
-        "cloud_name" => "diimgquir",
-        "api_key"    => "465976211781486",
-        "api_secret" => "_saOonEpr28eLpGGoql0kcF_vX0"
+        "cloud_name" => "x",
+        "api_key"    => "x",
+        "api_secret" => "x"
     ]
 ];
